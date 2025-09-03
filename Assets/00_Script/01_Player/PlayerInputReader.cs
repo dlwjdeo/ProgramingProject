c274e097b712d1992c503673de62096040ef19cf
btears25 <sksdlwjdeo@naver.com>
2025-09-03 19:57:01 +0900

    점프 구현

--- Assets/00_Script/01_Player/PlayerInputReader.cs
+++ Assets/00_Script/01_Player/PlayerInputReader.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +8,25 @@ public class PlayerInputReader : MonoBehaviour
 {
     private Vector2 move;
 
+    public event Action Jump;
+
     private void Update()
     {
         ReadMove();
+        DetectJump();
     }
 
     private void ReadMove()
     {
         move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void DetectJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump?.Invoke();
+        }
     }
 
 
--- Assets/00_Script/01_Player/PlayerMover.cs
+++ Assets/00_Script/01_Player/PlayerMover.cs
@@ -8,11 +8,14 @@ public class PlayerMover : MonoBehaviour
     [Header("이동")]
     [SerializeField] private float playerSpeed = 5f;
     [SerializeField] private float junpForce = 5f;
+    [SerializeField] private int maxJump = 1;
+    private int remainJunp;
 
     private Rigidbody2D _rigidbody2D;
     private PlayerInputReader _playerInputReader;
 
     private Vector2 move;
+    private bool jumpRequested = false;
 
     //플레이어가 계단을 이용할 때 x축 이동 방지용
     private bool moveX = true;
@@ -23,7 +26,9 @@ public class PlayerMover : MonoBehaviour
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _playerInputReader = GetComponent<PlayerInputReader>();
+        InitializeJump();
     }
+
 
     private void Update()
     {
@@ -33,6 +38,36 @@ public class PlayerMover : MonoBehaviour
     private void FixedUpdate()
     {
         _rigidbody2D.velocity = new Vector2(move.x * playerSpeed, _rigidbody2D.velocity.y);
+        TryJump();
+    }
+
+    private void OnEnable()
+    {
+        _playerInputReader.Jump += OnJunpPressed;
+    }
+
+    private void OnDisable()
+    {
+        _playerInputReader.Jump -= OnJunpPressed;
+    }
+
+    private void InitializeJump()
+    {
+        remainJunp = maxJump;
+    }
+
+    private void OnJunpPressed()
+    {
+        jumpRequested = true;
+    }
+
+    private void TryJump()
+    {
+        if(jumpRequested == false) return;
+        if (remainJunp <= 0) return;
+        _rigidbody2D.AddForce(Vector2.up * 5f, ForceMode2D.Impulse);
+        jumpRequested = false;
+        remainJunp--;
     }
 
     //API
