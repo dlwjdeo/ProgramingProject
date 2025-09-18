4639dc44543430bc3c3ff4fb7864720822ecf4d0
dlwjdeo <87020463+dlwjdeo@users.noreply.github.com>
2025-09-18 14:08:08 +0900

    포탈 구현(미완)

---
+++ Assets/00_Script/04_Object/00_Interact/Portal.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : Interactable
+{
+    [Header("포탈 이동 목표지점")]
+    [SerializeField] private Transform targetPoint;
+
+    public override void Interact()
+    {
+        if (player != null && targetPoint != null)
+        {
+            // 플레이어 위치 이동
+            player.transform.position = targetPoint.position;
+        }
+    }
+}
